d18f8380a12598001e72822cef88f1bace7246f5
Maria <32674641+MariaLisboa1@users.noreply.github.com>
2019-09-04 19:40:38 -0300

    atualiza

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 import { ProductsService } from "../products.service";
+import { ProductsItem } from "./products-item.model";
 
 @Component({
   selector: "app-home",
@@ -9,10 +10,35 @@ import { ProductsService } from "../products.service";
 export class HomePage implements OnInit {
   constructor(private product: ProductsService) {}
   prods;
+  public totalPrice: number = 0.0;
+  itemsInCart: Object[] = [];
+
+  cart: ProductsItem;
 
   ngOnInit() {
     this.product
       .products()
       .subscribe(res => (this.prods = res), err => console.log(err));
+  }
+
+  add(products) {
+    console.log(products);
+  }
+
+  updateTotalPrice(active: Boolean, pizzaPrice) {
+    active ? (this.totalPrice += pizzaPrice) : (this.totalPrice -= pizzaPrice);
+  }
+
+  getTotalPrice() {
+    return this.totalPrice;
+  }
+
+  addToCart(active: Boolean, id, pizza, description, price) {
+    if (active == true) {
+      this.cart.id = id;
+      this.cart.title = pizza;
+      this.cart.description = description;
+      console.log("aqui");
+    }
   }
 }
---
+++ src/app/home/products-item.model.ts
@@ -0,0 +1,13 @@
+export interface ProductsItem {
+  id: string;
+  aku: string;
+  title: string;
+  description: string;
+  availableSizes: string;
+  style: string;
+  price: number;
+  installments: number;
+  currencyId: string;
+  currencyFormat: string;
+  isFreeShipping: boolean;
+}
